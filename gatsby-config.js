b7640bed2e5db3862a06ecba12ee71c7ee66f4d2
Michael Hall <mohall42@students.tntech.edu>
2019-07-12 16:15:50 -0400

    Added material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,6 @@
 module.exports = {
   /* Your site config here */
 
-  plugins: [`gatsby-plugin-sass`]
+  plugins: [`gatsby-plugin-sass`,
+`gatsby-plugin-material-ui`]
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,31 +1,16 @@
 import React from 'react'
 import {Link } from 'gatsby'
 import logo from '../images/logo.png'
-import navStyles from './navbar.module.scss'
-import Menu from '../components/navIcon'
+
 
 
 const NavBar = () => {
  
-    
-    
-
     return(
-            <div >
-                <ul className = {navStyles.nav}>
-                     <li>
-                         <Link to='/'><img src={logo} alt = ''></img></Link>
+           <div>
 
-                     </li> 
-                     <li>
-                         <button className = {navStyles.icon}>
-                             <Menu />
-                         </button>
-                     
-                     </li>
-                </ul>
-            </div>
-            )
+           </div>
+        )
 }
 
 
