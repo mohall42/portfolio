096917a4827cfeaf21b86af39ec878701daad08d
Michael Hall <mohall42@students.tntech.edu>
2019-07-23 16:44:56 -0400

    Added more changes to banner

--- src/components/header.js
+++ src/components/header.js
@@ -18,6 +18,11 @@ const Head = styled.section `
 const Banner = styled.div`
     
     background: #434343;  /* fallback for old browsers */
+    background: #000000;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to right, #434343, #000000);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to right, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
 
     width: 90%;
     height: 50%;
@@ -27,8 +32,8 @@ const Banner = styled.div`
     left: 50%;
     margin-right: -50%; 
     transform: translate(-50%, -50%);
-    -webkit-clip-path: polygon(0 0, 1600px 0, 1600px 87%, 0 100%);
-    clip-path: polygon(0 0, 1600px 0, 1600px 87%, 0 100%);
+    -webkit-clip-path: polygon(0 13%, 100% 0, 100% 87%, 0 100%);
+    clip-path: polygon(0 13%, 100% 0, 100% 87%, 0 100%);
 
 
 
@@ -60,6 +65,8 @@ const Arrow = styled.img`
     height: 7%;
     transform: rotate(90deg);
     display: block;
+    margin-left: auto;
+    margin-right: auto;
     fill: green;
 
     
