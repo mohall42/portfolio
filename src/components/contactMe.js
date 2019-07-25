e8e7542e91e6b98026f487e63bf635f664d3f0ef
Michael Hall <mohall42@students.tntech.edu>
2019-07-25 13:10:51 -0400

    Now clear of any module.scss files

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -1,25 +1,61 @@
 import React from 'react'
+import styled from 'styled-components'
 import contactStyles from './contact.module.scss'
 import DribbbleLogo from '../images/dribbble.png'
 import GitHubLogo from '../images/github.png'
 import EmailLogo from '../images/email.png'
 
+
+const Container = styled.div` 
+
+    h2{
+        text-align: center;
+        color: white;
+        margin-top: 5%;
+        font-family: "Formular";
+    }
+
+    .symbol{
+
+        height: 10vmin;
+        width: 10vmin;
+       
+    }
+
+
+
+`;
+
+const Flex = styled.div` 
+
+    list-style: none;
+    display: flex;
+    justify-content: space-around;
+    margin: 10% 10%;   
+    
+
+
+
+
+`;
+
+
 const Contact = () => {
     return(
 
-        <div className={contactStyles.container}>
-            <h2 className={contactStyles.h2}>Get in touch with me</h2>
-            <div className ={contactStyles.flexbox}>
+        <Container>
+            <h2 class= "h2">Get in touch with me!!!</h2>
+            <Flex>
                 
-                <a  href= "https://dribbble.com/"><img className={contactStyles.symbol} src = {DribbbleLogo} alt=" "></img></a>
+                <a  href= "https://dribbble.com/"><img class = "symbol" src = {DribbbleLogo} alt=" "></img></a>
             
-                <a  href = "/"><img className={contactStyles.symbol} src = {GitHubLogo} alt=" "></img></a>
+                <a  href = "/"><img class="symbol" src = {GitHubLogo} alt=" "></img></a>
             
-                <a href = "/"><img className={contactStyles.symbol}  src = {EmailLogo} alt=" "></img></a>
+                <a href = "/"><img class = "symbol"  src = {EmailLogo} alt=" "></img></a>
             
                 
-            </div>
-        </div>
+            </Flex>
+        </Container>
 
         
 
